6f5a6601f9c53854b60487a2ab03332fba8ded33
Paul Sweeney <github@kolossi.co.uk>
2017-12-11 08:59:24 +0000

    day11

--- Runner/Day11.cs
+++ Runner/Day11.cs
@@ -6,14 +6,53 @@ namespace Runner
 {
     class Day11 : Day
     {
+        Dictionary<string, Func<XY, XY>> Offsets = new Dictionary<string, Func<XY, XY>>() {
+            { "n", xy=>new XY(xy.X, xy.Y+1) },
+            { "ne",xy=> new XY(xy.X+1, xy.Y+(xy.X % 2 == 0 ? 1 : 0)) },
+            { "se",xy=> new XY(xy.X+1, xy.Y-(xy.X % 2 == 0 ? 0 : 1)) },
+            { "s", xy=>new XY(xy.X, xy.Y-1) },
+            { "sw", xy=>new XY(xy.X-1,xy.Y-(xy.X % 2 == 0 ? 0 : 1) ) },
+            { "nw", xy=>new XY(xy.X-1, xy.Y+(xy.X % 2 == 0 ? 1 : 0)) },
+        };
+
         public override string First(string input)
         {
-            throw new NotImplementedException();
+            var dirs = input.Split(",");
+            var xy = new XY(0, 0);
+            foreach (var dir in dirs)
+            {
+                xy = Offsets[dir](xy);
+            }
+
+            return GetDistance(xy).ToString();
+        }
+
+        private int GetDistance(XY xy)
+        {
+            xy = new XY(xy.X, xy.Y);
+            int dist = 0;
+            while (xy.X!=0)
+            {
+                string dir = xy.X > 0 ? (xy.Y <= 0 ? "nw" : "sw") : (xy.Y <= 0 ? "ne" : "se");
+                xy = Offsets[dir](xy);
+                dist++;
+            }
+            dist += Math.Abs(xy.Y);
+            return dist;
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException();
+            var dirs = input.Split(",");
+            var xy = new XY(0, 0);
+            int maxDistance = 0;
+            foreach (var dir in dirs)
+            {
+                xy = Offsets[dir](xy);
+                maxDistance = Math.Max(maxDistance, GetDistance(xy));
+            }
+
+            return maxDistance.ToString();
         }
     }
 }
