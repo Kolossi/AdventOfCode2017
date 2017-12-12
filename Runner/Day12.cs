8ff622b2b35899157a715f86f668f26d2a557ef9
Paul Sweeney <github@kolossi.co.uk>
2017-12-12 06:46:37 +0000

    day12

--- Runner/Day12.cs
+++ Runner/Day12.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Runner
@@ -8,12 +9,66 @@ namespace Runner
     {
         public override string First(string input)
         {
-            throw new NotImplementedException();
+            var connections = GetConnections(input);
+            return GetConnected(0, connections).Count().ToString();
+        }
+
+        private List<int> GetConnected(int target, Dictionary<int, List<int>> connections)
+        {
+            var connected = new List<int>();
+            var toProcess = new Queue<int>();
+            toProcess.Enqueue(target);
+            connected.Add(target);
+
+            while (toProcess.Any())
+            {
+                var newProgId = toProcess.Dequeue();
+                var newProgs = connections[newProgId].Except(connected).ToArray();
+                connected.AddRange(newProgs);
+                foreach (var connectionId in newProgs)
+                {
+                    toProcess.Enqueue(connectionId);
+                }
+            }
+
+            return connected;
+        }
+
+        private static Dictionary<int, List<int>> GetConnections(string input)
+        {
+            var connections = new Dictionary<int, List<int>>();
+            var lines = GetLines(input);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(" <-> ");
+                var progId = int.Parse(parts[0]);
+                connections[progId] = new List<int>(parts[1].Split(", ").Select(w => int.Parse(w)));
+            }
+
+            return connections;
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException();
+            var connections = GetConnections(input);
+            return GetGroups(connections).ToString();
+        }
+
+        private int GetGroups(Dictionary<int, List<int>> connections)
+        {
+            var progGroup = new Dictionary<int, int>();
+            var toProcess = new Queue<int>();
+
+            while (progGroup.Keys.Count() < connections.Keys.Count())
+            {
+                var target = connections.Keys.Except(progGroup.Keys).First();
+                var connected = GetConnected(target, connections);
+                foreach (var connection in connected)
+                {
+                    progGroup[connection] = target;
+                }
+            }
+            return progGroup.Values.Distinct().Count();
         }
     }
 }
